3e6ba3ad253810632f93ff0dc0353b7bb6965db3
Will Rubin <willrubin1@gmail.com>
2023-10-05 15:09:26 -0400

    NavBar opens and closes

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import { RouterLink } from 'vue-router';
 
+const isActive = ref(false);
 </script>
 
 <template>
@@ -10,14 +13,14 @@
         <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="28" height="28" />
         </a>
 
-        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        <a role="button" class="navbar-burger" :class="{ 'is-active': isActive}" @click="isActive = !isActive" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
         <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
         </a>
     </div>
 
-    <div id="navbarBasicExample" class="navbar-menu">
+    <div id="navbarBasicExample" class="navbar-menu" :class="{ 'is-active': isActive }">
         <div class="navbar-start">
         <RouterLink class="navbar-item" to="/">Home</RouterLink>
         <RouterLink class="navbar-item" to="/about">About</RouterLink>
