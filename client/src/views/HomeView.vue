a356b4a8e210f581baa7468dd5087b07bc0a693e
Will Rubin <willrubin1@gmail.com>
2023-10-05 15:19:06 -0400

    added basic functionality

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,10 +1,46 @@
 <script setup lang="ts">
-
+import { ref } from 'vue';
+  const newTask = ref('');
+  const tasks = ref([] as string[]);
+  function addTask() {
+    tasks.value.push(newTask.value);
+    newTask.value = '';
+  };
 </script>
 
 <template>
   <main>
     <h1 class="title">Home</h1>
-    <h2 class="subtitle"> Welcome </h2>
+    <h2 class="subtitle"> Welcome to your app</h2>
+    <nav class="panel">
+    <p class="panel-heading">
+      Repositories
+    </p>
+    <div class="panel-block">
+      <p class="control has-icons-left">
+        <input class="input" type="text" placeholder="What do you want to do?" @keypress.enter="addTask" v-model="newTask">
+        <span class="icon is-left">
+          <i class="fas fa-plus" aria-hidden="true"></i>
+        </span>
+      </p>
+    </div>
+    <p class="panel-tabs">
+      <a class="is-active">All</a>
+      <a>Public</a>
+      <a>Private</a>
+      <a>Sources</a>
+      <a>Forks</a>
+    </p>
+    
+    <label class="panel-block" v-for="task in tasks">
+      <input type="checkbox">
+      {{ task }}
+    </label>
+    <div class="panel-block">
+      <button class="button is-link is-outlined is-fullwidth">
+        Reset all filters
+      </button>
+    </div>
+</nav>
   </main>
 </template>
