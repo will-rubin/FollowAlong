9ef0dae943800cfd0a179b9dbb5ccd3bdc35ac7a
Will Rubin <willrubin1@gmail.com>
2023-11-15 11:09:59 -0500

    added missing css to FlyoutPanel

--- client/src/components/FlyoutPanel.vue
+++ client/src/components/FlyoutPanel.vue
@@ -5,7 +5,6 @@
 <template>
     <div class="flyout">
         <slot></slot>
-
     </div>
 </template>
 
@@ -14,18 +13,32 @@
 <style scoped>
     .flyout {
         position: fixed;
-        top: 70px;
-        height: calc(100vh - 70px);
+        height: 100vh;
         width: 20rem;
+        top: 0;
         right: -18rem;
         background-color: white;
         border-left: 1px solid #ccc;
         box-shadow: 0 0 1rem #0004;
-        transition: right 0.5s ease-in-out;
+        transition: all 0.5s ease-in-out;
         z-index: 100;
     }
 
     .flyout:hover, .flyout.is_active {
         right: 0;
+    }
+
+    @media (max-width: 640px) {
+        .flyout {
+            width: 100vw;
+            height: 50vh;
+            left: 0;
+            right: 0;
+            top: 90vh;
+        }
+
+        .flyout:hover, .flyout.is_active {
+            top: 50vh;
+        }
     }
 </style>
