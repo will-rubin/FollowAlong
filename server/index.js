63ff1ffddd128012cbb7150f8452e94ff5525da4
Will Rubin <willrubin1@gmail.com>
2023-11-15 13:04:25 -0500

    added a few missing lines again

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 //vanilla node server w/ express
+const path = require('path'); //path module to help with file paths
 const express = require('express');  //CommonJS module syntax
 const productController = require('./controllers/products.js'); //import the router object from products.js
+const userController = require('./controllers/users.js'); //import the router object from users.js
 const app = express();  //instantiate express app, then create map of application routes
-const path = require('path'); //path module to help with file paths
 
 const mongo = require('./models/mongo.js');
 
@@ -12,28 +13,30 @@ const PORT = 3000;
 //CORS
 app 
   .use('/', express.static(path.join( __dirname, '../client/dist/')))
+
   .use(express.json())
-  .use('/api/v1/products', productController)
+
   .use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*'); //allow CORS
+    res.header('Access-Control-Allow-Methods', '*'); //allow CORS
     res.header('Access-Control-Allow-Headers', '*'); //allow CORS
     next()
   })
+
+  .use('/api/v1/products', productController)
+  .use('/api/v1/users', userController)
+
   .get('*', (req, res) => {
       res.sendFile(path.join(__dirname, '../client/dist/index.html'))
   })
-  .use((req, res ,next) => {
-    res.header('Access-Control-Allow-Origin', '*'); //allow CORS
-    res.header('Access-Control-Allow-Headers', '*'); //allow CORS
-    res.header('Access-Control-Allow-Headers', '*'); //allow CORS
-    next()
-  })
+
   .use((err, req, res, next) => { 
     console.log(err);
     res
       .status(err?.status || 500)
       .json({ message: err?.message || err })
   })
+
 console.log('1: Trying to start server...')
 
 app.listen(PORT, () => {
