db5fd660db515cb015df457a636829f0522a656a
Will Rubin <willrubin1@gmail.com>
2023-11-16 14:37:02 -0500

    dotenv working in server app

--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
 //vanilla node server w/ express
 const path = require('path'); //path module to help with file paths
 const express = require('express');  //CommonJS module syntax
+require('dotenv').config(); //dotenv module to help with environment variables - don't need const because we only need to load it once
 const productController = require('./controllers/products.js'); //import the router object from products.js
 const userController = require('./controllers/users.js'); //import the router object from users.js
 const app = express();  //instantiate express app, then create map of application routes
 
 const mongo = require('./models/mongo.js');
 
-const PORT = 3000;
+const PORT = process.env.PORT ?? 3000; //set port to 3000 if not specified in .env file - ?? is the null coalescer operator
+console.log(`The best class at SUNY New Paltz is ${process.env.BEST_CLASS}`); //access environment variables with process.env
 
 
 //CORS
--- server/models/mongo.js
+++ server/models/mongo.js
@@ -1,6 +1,6 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://newpaltz:suny@cluster0.ugduvlt.mongodb.net/?retryWrites=true&w=majority";
+const uri = process.env.MONGO_URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {});
