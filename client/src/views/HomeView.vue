88c978e56f02c0377236ce051b5978ca9ac56d04
Will Rubin <willrubin1@gmail.com>
2023-12-11 14:05:51 -0500

    typo fix

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { getUsers, type User } from '@/model/users';
+import { getUsers, type User } from '../model/users';
 import { ref } from 'vue';
 
   const newTask = ref('');
